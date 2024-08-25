f0372989f6b405fa035b55ec43e0276010397e11
bitten2up <bitten2up@courvix.com>
2024-08-24 19:01:54 -0500

    temperarry, currently only extracts the save for whatever reason

--- globals.h
+++ globals.h
@@ -11,6 +11,9 @@ typedef u8  bool8;
 typedef u16 bool16;
 typedef u32 bool32;
 
+#define TRUE  1
+#define FALSE 0
+
 #define MAX_PLAYER_NAME_LENGTH 6
 #define NUM_CHARACTERS 5
 
@@ -37,3 +40,75 @@ struct MultiplayerScore {
 struct TimeRecords {
     u16 table[NUM_CHARACTERS][NUM_COURSE_ZONES][ACTS_PER_ZONE][TIME_RECORDS_PER_COURSE];
 };
+
+// Used in struc_30008A0
+typedef struct {
+    u32 playerId;
+    u16 playerName[MAX_PLAYER_NAME_LENGTH];
+
+    bool8 slotFilled;
+
+    u8 wins;
+    u8 losses;
+    u8 draws;
+} VsRecords;
+
+// Used in struc_3000CF0
+typedef struct {
+    u8 unk0;
+    u8 wins;
+    u8 draws;
+    u8 losses;
+    u32 unk4;
+    u16 playerName[MAX_PLAYER_NAME_LENGTH];
+} VsRecords2;
+
+typedef struct {
+    u16 table[NUM_COURSE_ZONES][ACTS_PER_ZONE][TIME_RECORDS_PER_COURSE];
+} TimeRecords;
+typedef struct {
+    u32 magicNumber; // default: 0x47544E4C ("LNTG")
+    u32 v4;
+
+    // @NOTE This contains mostly the same information as struc_3000530, but
+    // some values are missing or in a different order.
+    u32 id; // | 0x08
+    u16 playerName[MAX_PLAYER_NAME_LENGTH];
+    u8 v18; // Don't know why this byte is here, but it's correct
+
+    u8 unlockedCharacters; // 0x19 |
+    u8 unlockedZones; // 0x1A |
+    u8 continueZone; // 0x1B | Zone the player continues at on startup after character
+                     // select (0 to 6)
+    u8 v1C;
+
+    u16 chaoCount[7]; // | v1D - 0x2B
+    u8 specialKeys[7]; // Each counter counts for 1 Act | 0x2C - 0x32
+    u8 unlockedStages[9]; // | 0x33-0x3B
+    u8 collectedEmeralds; // Stored bitwise: x7654321b | 0x3C
+
+    // NOTE: These are different from
+    //       struct struc_3000530, why?
+    u8 v3D; // v33: struc_3000530
+    u8 collectedMedals[9][4]; // 0x3E | v37: struc_3000530
+    u16 v62; // v34?: struc_3000530
+    u8 vsWins; // v60?: struc_3000530
+
+    u8 vsLosses;
+    u8 vsDraws;
+    u8 v67;
+
+    VsRecords2 vsRecords[10]; // 0x68
+
+    TimeRecords timeRecords[7][4][5]; // 0x130
+
+    u8 buttonConfig[3];
+    u8 v363;
+    u8 v364;
+    u8 v365;
+    u8 language;
+    u8 v367; // INVESTIGATE: v367 might be unused.
+
+    u32 checksum; // v368 | Checksum? In sub_800212C() this value is calculated and show that
+              // it HAS to be the last member of the struct
+} SaveSectorData;
--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
+#include <endian.h>
 
 #include "globals.h"
 
@@ -17,46 +18,122 @@
         reverse_bytes_32(checksum);                                                     \
     })
 
-#define SECTOR_SECURITY_NUM    0x4C45474
+#define UNLOCK_FLAG_CREAM       1 << 0
+#define UNLOCK_FLAG_TAILS       1 << 1
+#define UNLOCK_FLAG_KNUCKLES    1 << 2
+#define UNLOCK_FLAG_AMY         1 << 3
+#define UNLOCK_FLAG_BOSS_TA     1 << 4
+#define UNLOCK_FLAG_SOUND_TEST  1 << 5
+
+#define GAME_PLAY_OPTION_DIFFICULTY_EASY     1
+#define GAME_PLAY_OPTION_TIME_LIMIT_DISABLED 2
+
+#define PACKED_A_BUTTON 1
+#define PACKED_B_BUTTON 2
+#define PACKED_R_BUTTON 4
+
+#define SECTOR_SECURITY_NUM    0x47544E4C
 #define SECTOR_CHECKSUM_OFFSET 872
-#define NUM_SAVE_SECTORS       10
+#define NUM_SAVE_SECTORS       16
 
 
 int main(int argc, char* argv[])
 {
-    FILE* input = fopen("sa3.sav.bak", "r");
+    FILE* input = fopen("sa3.sav.bak1", "rb");
 
     fseek(input, 0L, SEEK_END);
     int inputsize = ftell(input);
     rewind(input);
-    
-    char* buffer = calloc(1, inputsize+1);
 
-    if (buffer == NULL)
+    s16 sectorNum, i, successfulRead;
+
+    i = 0;
+    // Keep a copy of the last game state
+
+    u32 maxVersion = 0, minVersion = 0xffffffff;
+    s16 bestSector = 0;
+
+    fseek(input, 4L, SEEK_CUR);
+    u32 curSector;
+
+    for (i = 0; i < NUM_SAVE_SECTORS; i++) {
+        char dammit[4];
+        fread(dammit, 4, 1, input);
+        curSector = *(u32*)dammit;
+        // If the value we just read was this
+        if (curSector > maxVersion) {
+            bestSector = i;
+            maxVersion = curSector;
+        }
+
+        if (curSector < minVersion) {
+            minVersion = curSector;
+        }
+        fseek(input, 4092L, SEEK_CUR);
+    }
+
+    if (minVersion > 0) {
+        sectorNum = bestSector;
+    }
+    else
+    {
+        // if we found a min version number of 0
+        // look through all the sectors for the highest
+        // version number until we get to a version number
+        // of <= 0xffff
+        maxVersion = 0;
+        for (i = 0; i < NUM_SAVE_SECTORS; i++) {
+            if (maxVersion <= 0xFFFF && curSector > maxVersion) {
+                sectorNum = i;
+                maxVersion = curSector;
+            }
+        }
+    }
+    printf("min version: %u\n", minVersion);
+    printf("max version: %u\n", maxVersion);
+    printf("best sector: %u\n", sectorNum);
+
+    fseek(input, 4096*bestSector, SEEK_SET);
+    
+    union data
+    {
+        char* buffer;
+        SaveSectorData *saveFile;
+    } save_u;
+
+    save_u.buffer = calloc(1, 4096+1);
+
+    if (save_u.buffer == NULL)
 	{
 		fclose(input);
-		free(buffer);
+		free(save_u.buffer);
 		fputs("mem alloc failed\n", stderr);
 		exit(1);
 	}
-    if (1 != fread(buffer,inputsize, 1, input))
+    if (1 != fread(save_u.buffer,4098, 1, input))
 	{
 		fclose(input);
-        free(buffer);
+        free(save_u.buffer);
 		fputs("read failed, or file is blank\n", stderr);
         exit(1);
 	}
+
     fclose(input);
-    u32 checksum = CalcChecksum(buffer);
-    printf("Valid Checksum is: %x\n", checksum);
-    buffer[SECTOR_CHECKSUM_OFFSET] = (checksum>>24) & 0xFF;
-	buffer[SECTOR_CHECKSUM_OFFSET+1] = (checksum>>16) & 0xFF;
-	buffer[SECTOR_CHECKSUM_OFFSET+2] = (checksum>>8) & 0xFF;
-	buffer[SECTOR_CHECKSUM_OFFSET+3] = checksum & 0xFF;
+
+    //SaveSectorData *saveFile = (SaveSectorData *)buffer;
+
+    //save_u.saveFile->language = 0;
+
+    for (i = 0; i<4092; i++)
+        printf("%c", save_u.buffer[i]);
+    printf("\n");
+
+    save_u.saveFile->checksum = CalcChecksum(save_u.buffer);
+    printf("Valid Checksum is: %x\n", save_u.saveFile->checksum);
     FILE* f1 = fopen("sa3.sav", "wb");
-    fwrite(buffer, sizeof(char), inputsize, f1);
+    fwrite(save_u.buffer, sizeof(char), 4098, f1);
     fclose(f1);
-    free(buffer);
-    buffer = NULL;
+    free(save_u.buffer);
+    save_u.buffer = NULL;
     return 0;
 }
